327c5c444dccf28155af47f018f05562143e0daf
S Kota <91359399+Skota11@users.noreply.github.com>
2023-01-12 22:45:25 +0900

    await_test

--- watch.js
+++ watch.js
@@ -1,34 +1,36 @@
-let url = new URL(window.location.href);
-let params = url.searchParams;
-const roomId = params.get("id");
+(async function main() {
+  let url = new URL(window.location.href);
+  let params = url.searchParams;
+  const roomId = params.get("id");
 
-const remoteVideo = document.getElementById("remote-stream");
+  const remoteVideo = document.getElementById("remote-stream");
 
-const peer = (window.peer = new Peer({
-  key: "ff4aac66-dfa3-41b1-8e9b-80fff81cd0c4",
-  debug: 3,
-}));
+  const peer = await (window.peer = new Peer({
+    key: "ff4aac66-dfa3-41b1-8e9b-80fff81cd0c4",
+    debug: 3,
+  }));
 
-const room = peer.joinRoom(roomId, {
-  mode: "mesh",
-});
-room.on("stream", async (stream) => {
-  const newVideo = document.createElement("video");
-  newVideo.srcObject = stream;
-  newVideo.playsInline = true;
-  newVideo.setAttribute("data-peer-id", stream.peerId);
-  newVideo.setAttribute("controls", "");
-  newVideo.setAttribute("class", "video-js");
-  newVideo.setAttribute("id", "videoPlayer");
-  remoteVideo.append(newVideo);
+  const room = peer.joinRoom(roomId, {
+    mode: "mesh",
+  });
+  room.on("stream", async (stream) => {
+    const newVideo = document.createElement("video");
+    newVideo.srcObject = stream;
+    newVideo.playsInline = true;
+    newVideo.setAttribute("data-peer-id", stream.peerId);
+    newVideo.setAttribute("controls", "");
+    newVideo.setAttribute("class", "video-js");
+    newVideo.setAttribute("id", "videoPlayer");
+    remoteVideo.append(newVideo);
 
-  videojs("videoPlayer", {
-    width: 640, // 幅
-    height: 360, // 高さ
-    autoplay: true, // 自動再生
-    loop: false, // ループ再生
-    controls: true, // コントロール制御表示
-    preload: "auto", // 読み込み制御
-    liveui: true,
-  }).play();
-});
+    videojs("videoPlayer", {
+      width: 640, // 幅
+      height: 360, // 高さ
+      autoplay: true, // 自動再生
+      loop: false, // ループ再生
+      controls: true, // コントロール制御表示
+      preload: "auto", // 読み込み制御
+      liveui: true,
+    }).play();
+  });
+})();
